b98190c006b776c55a688cb1ad8da15b9bd7e8b4
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2022-01-07 17:16:18 +0500

    Создал заготовку для для search

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,12 @@
 import appStyles from "./app.module.css";
 import Header from "../header/header";
-
+import Search from "../search/search";
 function App() {
   return (
     <div className={appStyles.app}>
       <div className={appStyles.content}>
         <Header />
+        <Search />
       </div>
     </div>
   );
---
+++ src/components/search/search.js
@@ -0,0 +1,7 @@
+import searchStyles from "./search.module.css";
+import { FaSearch } from "react-icons/fa";
+function Search() {
+  return <FaSearch />;
+}
+
+export default Search;
