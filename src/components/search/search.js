5f51b49ed1478b3d792bbf8e92d3aa798a726b45
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2022-01-08 11:13:18 +0500

    сверстал поиск

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,7 +1,25 @@
 import searchStyles from "./search.module.css";
 import { FaSearch } from "react-icons/fa";
 function Search() {
-  return <FaSearch />;
+  return (
+    <>
+      <form className={searchStyles.form}>
+        <div className={searchStyles.inputContainer}>
+          <FaSearch />
+          <input
+            type="text"
+            name="search_text"
+            placeholder="Введите данные поиска"
+            className={searchStyles.input}
+          />
+        </div>
+
+        <button type="submit" className={searchStyles.button}>
+          Поиск
+        </button>
+      </form>
+    </>
+  );
 }
 
 export default Search;
