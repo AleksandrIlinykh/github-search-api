aaa9f219608c45dc5678c16d5db0266dbb531d53
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2022-01-08 11:13:35 +0500

    подключил карточки и поиск

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,20 @@
 import appStyles from "./app.module.css";
 import Header from "../header/header";
 import Search from "../search/search";
+import Cards from "../cards/cards";
 function App() {
   return (
     <div className={appStyles.app}>
       <div className={appStyles.content}>
-        <Header />
-        <Search />
+        <div className={appStyles.headerContainer}>
+          <Header />
+        </div>
+        <div className={appStyles.searchContainer}>
+          <Search />
+        </div>
+        <div className={appStyles.cardsContainer}>
+          <Cards />
+        </div>
       </div>
     </div>
   );
