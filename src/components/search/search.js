08066a69a605fa85a3c0bdae442e22552426caa4
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2022-01-09 12:19:01 +0500

    Первый запрос на API

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,9 +1,31 @@
-import searchStyles from "./search.module.css";
+import { createRef } from "react";
+import axios from "axios";
 import { FaSearch } from "react-icons/fa";
+
+import searchStyles from "./search.module.css";
+
 function Search() {
+  const input = createRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (input.current.value) {
+      const searchUsers = async (query, page = 0, per_page = 12) => {
+        await axios.get("https://api.github.com/search/users", {
+          params: {
+            q: query,
+            per_page: per_page,
+            page: page,
+          },
+        });
+      };
+
+      searchUsers(input.current.value);
+    }
+  };
+
   return (
     <>
-      <form className={searchStyles.form}>
+      <form className={searchStyles.form} onSubmit={handleSubmit}>
         <div className={searchStyles.inputContainer}>
           <FaSearch />
           <input
@@ -11,6 +33,7 @@ function Search() {
             name="search_text"
             placeholder="Введите данные поиска"
             className={searchStyles.input}
+            ref={input}
           />
         </div>
 
